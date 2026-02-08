7d81805a71f6d6e9d8a7c443c23485f61fbe7734
mostafizar-pimjo <mostafizar.pimjo@gmail.com>
2026-02-08 17:17:18 +0600

    skillsaint-free site

---
+++ src/components/footer/copyright.tsx
@@ -0,0 +1,15 @@
+const Copyright = () => {
+  return (
+    <div className="border-t border-t-gray-800 py-6">
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <p className="max-w-[586px] text-center text-xs leading-4.5 tracking-base text-gray-500">
+            © Skillsaint 2025. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Copyright;
